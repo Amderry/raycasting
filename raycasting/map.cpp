ae41d487b2bd6eeff8887b787c8c2c7b75c441c3
Alex1934 <alez1934@yandex.ru>
2025-10-20 17:11:17 +0300

    изменить: вынести логику определения коллизий в класс Map А также добавить геттеры сеттеры для полей, отвечающих за размер карты и чанка

--- raycasting/map.cpp
+++ raycasting/map.cpp
@@ -2,8 +2,8 @@
 
 Map::Map()
 {
-  map_array = NULL;
-  map_length = 0;
+  m_map_array = NULL;
+  m_length = 0;
 }
 
 int Map::load(const std::string map_filename)
@@ -15,12 +15,12 @@ int Map::load(const std::string map_filename)
     return 1;   
   }
 
-  map_file >> map_length;
-  map_array = new int[map_length];
+  map_file >> m_length;
+  m_map_array = new int[m_length];
 
-  for(int i = 0; i < map_length; ++i)
+  for(int i = 0; i < m_length; ++i)
   {
-    map_file >> map_array[i];
+    map_file >> m_map_array[i];
   }
   map_file.close();
 
@@ -34,29 +34,59 @@ Map::~Map()
 
 void Map::unload()
 {
-  map_length = 0;
-  delete[] map_array;
-  map_array = NULL;
+  m_length = 0;
+  delete[] m_map_array;
+  m_map_array = NULL;
 }
 
 int* Map::get_map()
 {
-  return map_array;
+  return m_map_array;
 }
 
-int Map::get_map_length()
+int Map::get_m_length()
 { 
-  return map_length;
+  return m_length;
+}
+
+void Map::set_vertical_size(int vsize)
+{
+  m_vertical_size = vsize;
+}
+
+int Map::get_vertical_size()
+{
+  return m_vertical_size;
+}
+
+void Map::set_horizontal_size(int hsize)
+{
+  m_horizontal_size = hsize;
+}
+
+int Map::get_horizontal_size()
+{
+  return m_horizontal_size; 
+}
+
+void Map::set_chunk_size(int csize)
+{
+  m_chunk_size = csize;
+}
+
+int Map::get_chunk_size()
+{
+  return m_chunk_size;
 }
 
 void Map::draw_map(Image& image, const Uint32 width, const Uint32 height)
 {
-  for (int y = 0; y < 8; ++y)
+  for (int y = 0; y < m_vertical_size; ++y)
   {
-    for (int x = 0; x < 8; ++x)
+    for (int x = 0; x < m_horizontal_size; ++x)
     {
       double red, green, blue;
-      if (map_array[x + y * 8])
+      if (m_map_array[x + y * m_chunk_size])
       { 
         red = static_cast<double>(90 - y)/90.0 * 255;
         blue = static_cast<double>(90 - y)/90.0 * 255;
@@ -68,10 +98,22 @@ void Map::draw_map(Image& image, const Uint32 width, const Uint32 height)
         blue = 0.0;
         green = 0.0;
       }
-      for (int j = 0; j < 64; ++j)
+      for (int j = 0; j < m_vertical_size * m_horizontal_size; ++j)
       {
-        image.set_pixel(x * 8 + j % 8, y * 8 + j / 8, red, green, blue);
+        image.set_pixel(x * m_chunk_size + j % m_chunk_size, y * m_chunk_size + j / m_chunk_size, red, green, blue);
       }
     }
+  }
+}
+
+bool Map::is_colliding(int dx, int dy)
+{
+  if ((dx / m_chunk_size >= 0 && dx / m_chunk_size <= (m_horizontal_size - 1)) && (dy / m_chunk_size >= 0 && dy / m_chunk_size <= (m_vertical_size - 1)))
+  {
+    return m_map_array[dy / m_chunk_size * m_vertical_size + dx / m_chunk_size];
+  }
+  else
+  {
+    return true;
   }
 }
--- raycasting/map.hpp
+++ raycasting/map.hpp
@@ -20,13 +20,24 @@ class Map
     void unload();
 
     int* get_map();
-    int get_map_length();
+    int get_m_length();
+
+    void set_vertical_size(int vsize);
+    int get_vertical_size();
+    void set_horizontal_size(int hsize);
+    int get_horizontal_size();
+    void set_chunk_size(int csize);
+    int get_chunk_size();
 
     void draw_map(Image& image, const Uint32 width, const Uint32 height);
+    bool is_colliding(int dx, int dy);
 
   private:
-    int *map_array;
-    int map_length;
+    int *m_map_array;
+    int m_length;
+    int m_vertical_size;
+    int m_horizontal_size;
+    int m_chunk_size;
 };
 
 #endif
